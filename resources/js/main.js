af6f157ddd8708069b8164e082b0d7efe86d278d
parmisnaddaf <parmisnaddaf@yahoo.com>
2019-07-30 18:29:15 -0700

    added alertify

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,2 +1,9 @@
+add_button = document.getElementById('addbutton');
+item = document.getElementById('item');
+console.log(add_button);
 
-console.log("BOOOOOOOOOOOOO");
+add_button.addEventListener ("click", function() {
+  if (item.value === ''){
+    alert("you must enter an activity first");
+  }
+});
